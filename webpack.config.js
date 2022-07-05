56f7171378a3be6fb98ca2ae3d76dfa5672494b5
ndowkunda <mn057@outlook.com>
2022-07-05 17:58:23 +0100

    add initial setup

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    title: 'Development',
+  }),
+  ]
+};
